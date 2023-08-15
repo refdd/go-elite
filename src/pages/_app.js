d871cea2a02634810ed378add005e13084edf0fa
RefatAli <mohamedrefat8434@gmail.com>
2023-08-15 14:28:16 +0300

    gtm remove

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,5 @@
 import "@/styles/globals.css";
-import Script from "next/script";
-import { useEffect } from "react";
-export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    // Google Analytics
-    const script = document.createElement("script");
-    script.src = `https://www.googletagmanager.com/gtag/js?id=GTM-P7GBDH9`;
-    script.async = true;
-    document.head.appendChild(script);
 
-    script.onload = () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        window.dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "GTM-P7GBDH9");
-    };
-  }, []);
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,35 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-P7GBDH9');
-              `,
-          }}
-        />
-      </Head>
-      <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-P7GBDH9`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <Main />
+      <Head />
 
+      <body>
+        <Main />
         <NextScript />
       </body>
     </Html>
