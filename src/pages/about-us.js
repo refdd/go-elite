7219408fbd9595c0f3b456e38d4d71f1a3673508
RefatAli <mohamedrefat8434@gmail.com>
2023-08-22 06:14:01 +0300

    push to see all

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -4,9 +4,11 @@ import React from "react";
 import HeaderSingBlog from "@/components/blog/HeaderSingBlog";
 import Footer from "@/components/Footer/Footer";
 import { baseUrl, fetchApi } from "../../utils/featchApi";
-import FAQs from "@/components/FAQs/FAQs";
-function FAndQ({ menus, logo, footer, socials, faq_page, faqs }) {
-  const { image, title, meta } = faq_page;
+import OverviewAboutus from "@/components/Aboutus/OverviewAboutus";
+
+function Aboutus({ menus, logo, footer, socials, about_page }) {
+  const { image, title, meta, body } = about_page;
+  console.log(about_page);
   return (
     <div>
       {" "}
@@ -18,19 +20,17 @@ function FAndQ({ menus, logo, footer, socials, faq_page, faqs }) {
       </Head>
       <NavBar menus={menus} logo={logo} />
       <HeaderSingBlog imgsrc={image?.image_url} title={title} />
-      <div className="mt-10">
-        <FAQs faqs={faqs} faq_page={true} />
-      </div>
+      <OverviewAboutus title={title} body={body} />
       <Footer logo={logo} footer={footer} socials={socials} />
     </div>
   );
 }
 
-export default FAndQ;
+export default Aboutus;
 export async function getStaticProps() {
   const menus = await fetchApi(`${baseUrl}/menus?tenant_id=9&language_id=5`);
-  const faq_page = await fetchApi(
-    `${baseUrl}/pages/faq_page?tenant_id=9&language_id=5`
+  const about_page = await fetchApi(
+    `${baseUrl}/pages/about-us?tenant_id=9&language_id=5`
   );
   const logo = await fetchApi(
     `${baseUrl}/settings/logo?tenant_id=9&language_id=5`
@@ -41,17 +41,14 @@ export async function getStaticProps() {
   const socials = await fetchApi(
     `${baseUrl}/socials?tenant_id=9&language_id=5&status=active`
   );
-  const faqs = await fetchApi(
-    `${baseUrl}/faqs?tenant_id=9&language_id=5&viewInHome=1&status=active&paginate=10`
-  );
+
   return {
     props: {
       menus: menus.rows,
       logo: logo.row,
-      faq_page: faq_page.row,
+      about_page: about_page.row,
       footer: footer.rows,
       socials: socials.rows,
-      faqs: faqs.rows,
     },
     revalidate: 10,
   };
